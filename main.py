07649d73472679deebf1d823e5dcacb39d768b8c
tombrov <tbrovy@gmail.com>
2020-11-10 20:04:49 +0200

    main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from solver import solve, valid, print_board
+from solver import solve, print_board
 from boards import board_1, board_2, board_3
+
 
 boards = {"easy": board_1, "medium": board_2, "hard": board_3}
 b = input("Please choose your level\n"
